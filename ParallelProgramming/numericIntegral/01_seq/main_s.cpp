49e448c6429cec3af0f87941bd80eabe61fb7cee
Anatoly Solomonov <solomonov010@mail.ru>
2024-02-11 17:18:38 +0400

    Добавлена формула Симпсона для вычисления интеграла

--- ParallelProgramming/numericIntegral/01_seq/main_s.cpp
+++ ParallelProgramming/numericIntegral/01_seq/main_s.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
-
-// Число pi
-#define PI 3.1415926535897932384626433832795
+#include <omp.h>
 
 using namespace std;
+
+// Рассматриваемая функция
+double func(double x) {
+	return 1.0 / (1.0 + x * x);
+}
 
 void integral(const double a, const double b,
 	const double h, double* res)
 {
 	int i, n;
-	double sum;				// локальная переменная для подсчета интеграла
-	double x;				// координата точки сетки
 	n = (int)((b - a) / h); // количество точек сетки интегрирования
-	// Цикл вычисления интеграла:
-	sum = 0.0;
-	for (i = 0; i < n; i++)
-	{
-		x = a + i * h + h / 2.0;
-		sum += sin(x) * h;
-	}
-	*res = sum;
+	// Цикл вычисления интеграла (Формула Симпсона):
+	// Лоакальные переменные для значений сумм внутри формуле
+	double sum1 = 0.0, sum2 = 0.0;
+	double height = (b - a) / (2 * n);	// Высота трапеции
+	for (i = 1; i < n; i++)
+		sum1 += func(a + (2 * i - 1) * height);
+	for (i = 1; i < n - 1; i++)
+		sum2 += func(a + 2 * i * height);
+	*res = (height / 3) * (func(a) + func(b) + 4 * sum1 + 2 * sum2);
 }
 
 double experiment(double* res)
 {
 	double stime, ftime;	// время начала и конца расчета
 	double a = 0.0;			// левая граница интегрирования
-	double b = PI;			// правая граница интегрирования
-	double h = 0.0000001;	// шаг интегрирования
+	double b = 1e6;			// правая граница интегрирования
+	double h = 0.001;		// шаг интегрирования
 	stime = clock();
 	integral(a, b, h, res);	// вызов функции интегрирования
 	ftime = clock();
@@ -42,7 +44,7 @@ int main()
 	double min_time;	// минимальное время работы реализации алгоритма
 	double max_time;	// максимальное время работы реализации алгоритма
 	double avg_time;	// среднее время работы реализации алгоритма
-	int numbExp = 10; // количество запусков программы
+	int numbExp = 3;	// количество запусков программы
 	// первый запуск
 	min_time = max_time = avg_time = experiment(&res);
 	// оставшиеся запуски
